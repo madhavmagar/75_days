0f6371d008b29c9935655646cccb40e31713c509
Madhav <msmagar2002@gmail.com>
2023-09-12 10:53:37 +0530

    dart basics

--- dart/basic.dart
+++ dart/basic.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class Basic {
   var names = ["mad", "kazuya", "akari", "jirou", "mizuhara"];
 
@@ -9,6 +11,19 @@ class Basic {
 }
 
 void main() {
+  // print("hello world!");
+
+  // // int? num;
+
+  // // assert(num == null);
+
+  // // num = 2000;
+  // var num = stdin.readLineSync();
+
+  // stdout.write("$num \n");
+  // int? s = int.parse(num!);
+  // stdout.write(s.runtimeType);
+
   var bs = new Basic();
 
   bs.pn();
